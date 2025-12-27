70ab70c6a6536307505710dcd2c2ceb9bbee5ec5
William Nkosi <williamnkosi@Williams-MacBook-Pro.local>
2025-12-26 19:27:40 -0500

    removed search bar

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,7 @@ import SearchBar from "./components/search_bar";
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen  justify-items-start bg-zinc-50 font-sans ">
-      <div className="justify-center flex row mt-8">
-        <SearchBar />
-      </div>
+      <div className="justify-center flex row mt-8">{/* <SearchBar /> */}</div>
 
       <div className="flex flex-col justify-start mt-16 px-32">
         <div className="border-b-2 border-zinc-800 pb-2">
