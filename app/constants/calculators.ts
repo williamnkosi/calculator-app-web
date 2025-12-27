bf3c77a39ed4923b1cb558f338a27424c49d3547
William Nkosi <williamnkosi@Williams-MacBook-Pro.local>
2025-12-26 19:27:08 -0500

    Removed the uneeded calculators

--- app/constants/calculators.ts
+++ app/constants/calculators.ts
@@ -1,25 +1,19 @@
 const financialCalculatorTypes = [
   {
     id: 1,
-    title: "Mortgage Calculator",
-    route: "/pages/calculators/finanace/mortage",
-  },
-  {
-    id: 2,
     title: "Investment Calculator",
     route: "/pages/calculators/finanace/interest",
   },
   {
-    id: 3,
+    id: 2,
     title: "Loan Calculator",
     route: "/pages/calculators/finanace/loan",
   },
   {
-    id: 4,
+    id: 3,
     title: "Savings Calculator",
     route: "/pages/calculators/finanace/savings",
   },
-  { id: 5, title: "Tax Calculator", route: "/pages/calculators/finanace/tax" },
 ];
 
 export default financialCalculatorTypes;
